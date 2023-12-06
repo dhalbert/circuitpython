7f953253e517749abf2147da073f4ff50dd83ce6
Dan Halbert <halbert@halwitz.org>
2023-12-05 21:10:38 -0500

    wip

--- shared-bindings/btio/__init__.h
+++ shared-bindings/btio/__init__.h
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) 2023 Dan Halbert for Adafruit Industries
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,19 +24,11 @@
  * THE SOFTWARE.
  */
 
-#ifndef SHARED_BINDINGS_USB_HID_H
-#define SHARED_BINDINGS_USB_HID_H
+#pragma once
 
-#include "py/obj.h"
-#include "py/objtuple.h"
-#include "shared-module/usb_hid/__init__.h"
+#include "shared-bindings/btio/HID.h"
 
-extern mp_obj_tuple_t common_hal_usb_hid_devices;
+extern btio_hid_obj_t common_hal_btio_hid_obj;
 
-void usb_hid_set_devices(mp_obj_t devices);
-
-bool common_hal_usb_hid_disable(void);
-bool common_hal_usb_hid_enable(const mp_obj_t devices_seq, uint8_t boot_device);
-uint8_t common_hal_usb_hid_get_boot_device(void);
-
-#endif // SHARED_BINDINGS_USB_HID_H
+void common_hal_btio_init(void)
+void common_hal_btio_gc_collect(void);
